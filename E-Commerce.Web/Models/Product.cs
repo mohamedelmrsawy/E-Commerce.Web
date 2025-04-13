c4cc3ee31a80098f4da5d2c2eae44b40efc481c2
mohamed elmrsawy <mohamedelmrsawy@outlook.com>
2025-04-13 19:08:08 +0200

    2. APIs Types

---
+++ E-Commerce.Web/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce.Web.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+    }
+}
