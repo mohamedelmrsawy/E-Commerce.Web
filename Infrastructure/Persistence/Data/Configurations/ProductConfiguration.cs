846d5d9e049a4af3123c10d7e244301b63cd7b89
mohamed elmrsawy <mohamedelmrsawy@outlook.com>
2025-04-14 18:33:18 +0200

    5. Product Configurations && Db Context

---
+++ Infrastructure/Persistence/Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using DomianLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Data.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasOne(p => p.productBrand).WithMany().HasForeignKey(p => p.BrandId);
+            builder.HasOne(p => p.productType).WithMany().HasForeignKey(p => p.TypeId);
+            builder.Property(p => p.Price).HasColumnType("decimal(10,2)");
+        }
+    }
+}
