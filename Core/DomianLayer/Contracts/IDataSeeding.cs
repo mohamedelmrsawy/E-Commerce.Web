ea2be4eff2abfa294803040b87739618a0135343
mohamed elmrsawy <mohamedelmrsawy@outlook.com>
2025-04-19 23:53:34 +0200

    1,2,3 . Generic Repository

--- Core/DomianLayer/Contracts/IDataSeeding.cs
+++ Core/DomianLayer/Contracts/IDataSeeding.cs
@@ -8,6 +8,6 @@ namespace DomianLayer.Contracts
 {
     public interface IDataSeeding
     {
-        void DataSeed();
+        Task DataSeedAsync();
     }
 }
---
+++ Infrastructure/Persistence/Repository/GenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using DomianLayer.Contracts;
+using DomianLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repository
+{
+    public class GenericRepository<T, TKey>(StorDbContext _dbContext) : IGenericRepository<T, TKey> where T : BaseEntity<TKey>
+    {
+        public async Task AddAsync(T entity) => await _dbContext.Set<T>().AddAsync(entity);
+    
+        public async Task<IEnumerable<T>> GetAllAsync() => await _dbContext.Set<T>().ToListAsync();
+
+        public async Task<T?> GetbyIdAsync(TKey id) => await _dbContext.Set<T>().FindAsync(id);
+
+        public void Remove(T entity) => _dbContext.Set<T>().Remove(entity);
+
+        public void Update(T entity) => _dbContext.Set<T>().Update(entity);
+
+    }
+}
