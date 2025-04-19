9da98f974dd5e24134b59b1ef9db676efc571152
mohamed elmrsawy <mohamedelmrsawy@outlook.com>
2025-04-20 01:43:08 +0200

    4. UnitOfWork

---
+++ Core/DomianLayer/Contracts/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using DomianLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomianLayer.Contracts
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<T, TKey> GetRepository<T, TKey>() where T : BaseEntity<TKey>;
+        Task<int> SaveChangesAsync();
+    }
+}
