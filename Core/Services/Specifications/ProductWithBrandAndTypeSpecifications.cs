520682d8be93334546667eba44601f15916bd3cb
mohamed elmrsawy <mohamedelmrsawy@outlook.com>
2025-04-21 21:56:10 +0200

    3. Specifications All Product

---
+++ Core/Services/Specifications/ProductWithBrandAndTypeSpecifications.cs
@@ -0,0 +1,24 @@
+﻿using DomianLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Specifications
+{
+    internal class ProductWithBrandAndTypeSpecifications : BaseSpecifications<Product , int>
+    {
+        public ProductWithBrandAndTypeSpecifications():base(null)
+        {
+            AddInclude(P => P.productBrand);
+            AddInclude(P => P.productType);
+        }
+
+        public ProductWithBrandAndTypeSpecifications(int id):base(p=>p.Id == id) 
+        {
+            AddInclude(P => P.productBrand);
+            AddInclude(P => P.productType);
+        }
+    }
+}
