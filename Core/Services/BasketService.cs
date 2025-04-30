8dfd5664b4f92f71f70236f4109255d51aa960ea
mohamed elmrsawy <mohamedelmrsawy@outlook.com>
2025-04-30 17:39:35 +0300

    12. Basket Service

---
+++ Core/Services/BasketService.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using DomianLayer.Contracts;
+using DomianLayer.Exceptions;
+using DomianLayer.Models.BasketModules;
+using ServiceAbstraction;
+using Shared.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class BasketService(IBasketRepository _basketRepository, IMapper _mapper) : IBasketService
+    {
+        public async Task<BasketDto> CreateOrUpdateBasketAsync(BasketDto basket)
+        {
+            var CustomerBasket = _mapper.Map<BasketDto, CustomerBasket>(basket);
+            var CreateOrUpdatedBasket =await _basketRepository.CreateOrUpdateBasketAsync(CustomerBasket);
+            if (CreateOrUpdatedBasket is not null)
+                return await GetBasketAsync(basket.Id);
+            else
+                throw new Exception("!!!!!!!!!!!!!");
+        }
+
+        public async Task<bool> DeletedBasketAsync(string Key) => await _basketRepository.DeleteBasketAsync(Key);
+        
+
+        public async Task<BasketDto> GetBasketAsync(string Key)
+        {
+            var basket =await _basketRepository.GetBasketAsync(Key);
+            if (basket is not null)
+                return _mapper.Map<CustomerBasket, BasketDto>(basket);
+            else
+                throw new BasketNotFoundException(Key);
+        }
+    }
+}
---
+++ Core/Services/MappingProfile/BasketProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using DomianLayer.Models.BasketModules;
+using Shared.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.MappingProfile
+{
+    internal class BasketProfile : Profile
+    {
+        public BasketProfile()
+        {
+            CreateMap<CustomerBasket,BasketDto>().ReverseMap();
+            CreateMap<BasketItem, BasketItemDto>().ReverseMap();
+        }
+    }
+}
