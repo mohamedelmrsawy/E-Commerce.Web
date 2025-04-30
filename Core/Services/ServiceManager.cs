0e6c4af7aac0770253e1f8492266b3b0127cb6d4
mohamed elmrsawy <mohamedelmrsawy@outlook.com>
2025-04-30 19:34:41 +0300

    13. Basket Controller

--- Core/Services/ServiceManager.cs
+++ Core/Services/ServiceManager.cs
@@ -9,9 +9,12 @@ using System.Threading.Tasks;
 
 namespace Services
 {
-    public class ServiceManager(IUnitOfWork _unitOfWork,IMapper _mapper) : IServiceManager
+    public class ServiceManager(IUnitOfWork _unitOfWork,IMapper _mapper, IBasketRepository basketRepository) : IServiceManager
     {
         private readonly Lazy<IProductService> _LazyproductService = new Lazy<IProductService>(() => new ProductService(_unitOfWork, _mapper));
         public IProductService productService => _LazyproductService.Value;
+
+        private readonly Lazy<IBasketService> _LazyBasketService = new Lazy<IBasketService>(() => new BasketService(basketRepository, _mapper));
+        public IBasketService basketService => _LazyBasketService.Value;
     }
 }
