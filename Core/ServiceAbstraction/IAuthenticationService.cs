8737cfb1843d5284a311d3c457303599b9054b2c
mohamed elmrsawy <mohamedelmrsawy@outlook.com>
2025-05-04 13:02:55 +0300

    09/ Authentication Service

--- Core/ServiceAbstraction/IAuthenticationService.cs
+++ Core/ServiceAbstraction/IAuthenticationService.cs
@@ -11,5 +11,9 @@ namespace ServiceAbstraction
     {
         Task<UserDto> LoginAsync(LoginDto loginDto);
         Task<UserDto> RegisterAsync(RegisterDto registerDto);
+        Task<bool> CheckEmailAsync(string email);
+        Task<AddressDto> GetCurrentUserAddressAsync(string Email);
+        Task<AddressDto> UpdateCurrentUserAddressAsync(AddressDto addressDto , string Email);
+        Task<UserDto> GetCurrentUserAsync(string Email);
     }
 }
